d8df2a5828eca2dd6ef37d7246f1e7d3e84f6107
Andrew Mukare <dremukare@gmail.com>
2021-02-03 13:56:29 +0000

    made a fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,7 @@
 */
 int main(void)
 {
-	char output[];
-
-	output = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	fprintf(stderr, output);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
+	fprintf(stderr, "2015-10-19\n");
 	return (1);
 }
