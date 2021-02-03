0801d5296a58c8b877019393bf677e817d892fba
Andrew Mukare <dremukare@gmail.com>
2021-02-03 12:34:12 +0000

    wrote c programs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - This function prints out the string
+* "Programming is like building a multilingual puzzle
+* Return: Function returns 0
+*/
+int main(void)
+{
+	char output_string[]
+
+	output_string = "\"Programming is like building a multilingual puzzle";
+	puts(output_string);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - This function prints out the string
+* with proper grammar, but the outcome is a piece of art,
+* Return: This function returns 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, ");
+	return (0);
+}
