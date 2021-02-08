68dcbb117b1f9b75c6c6b63898b9ade39cbe0698
Andrew Mukare <dremukare@gmail.com>
2021-02-08 14:51:55 +0000

    completed header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -27,3 +27,33 @@ int _isalpha(int c);
 * print_sign - prints the sign of a number
 */
 int print_sign(int n);
+
+/** 
+* _abs - computes absolute value of integer
+*/
+int _abs(int);
+
+/**
+* print_last_digit - returns last digit of a number
+*/
+int print_last_digit(int);
+
+/**
+* jack_bauer - prints every minute of every day of jack bauer
+*/
+void jack_bauer(void);
+
+/**
+* times_table - prints the 9 times table starting with 0
+*/
+void times_table(void);
+
+/**
+* add - adds two integers and returns result
+*/
+int add(int, int);
+
+/**
+* print_to_98 - prints all natural numbers from n to 98 followed by a new line
+*/
+void print_to_98(int n);
