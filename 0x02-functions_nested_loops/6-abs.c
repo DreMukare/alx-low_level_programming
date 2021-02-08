514fb303909b5851c4fd757d6bba3f000b5fdc58
Andrew Mukare <dremukare@gmail.com>
2021-02-08 15:01:11 +0000

    attempted solutions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* _abs - computers absolute value of an integer
+* @int: integer to be passed
+*
+* Return: function returns 0
+*/
+int _abs(int)
+{
+	int = int * -1;
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "header.h"
+
+/**
+* print_last_digit - returns value of last digit of a number
+* @int: integer to be passed
+*
+* Return: reutns last digit of number
+*/
+int print_last_digit(int)
+{
+	int = int % 10;
+	return (int);
+}
