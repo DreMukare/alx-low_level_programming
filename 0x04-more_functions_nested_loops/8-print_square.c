8aae5f054fe9e7380a9b9b86df0b86c17b74cf30
Andrew Mukare <dremukare@gmail.com>
2021-02-11 14:45:10 +0000

    draw square on terminal

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,19 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else if (size <= 0)
+	{
 		_putchar('\n');
 	}
 }
