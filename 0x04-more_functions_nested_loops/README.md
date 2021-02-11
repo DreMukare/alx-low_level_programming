8abefd2cadd1c5a801e43fc1706cf09739cf3ce9
Andrew Mukare <dremukare@gmail.com>
2021-02-11 08:12:20 +0000

    initial commit

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# 0x04 - More functions, more nested loops
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,61 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+* _isupper - checks for uppercase character
+*/
+int _isupper(int c);
+
+/**
+* _isdigit - checks for digit (0 through 9
+*/
+int _isdigit(int c);
+
+/**
+* mul - multiplies two integers
+*/
+int mul(int a, int b);
+
+/**
+* print_numbers - prints the numbers from 0 to 9 
+*/
+void print_numbers(void);
+
+/**
+* print_most_numbers - print numbers from 0 to 9 except 2 and 4
+*/
+void print_most_numbers(void);
+
+/**
+* more_numbers - prints 10 times the numbers form 0 to 14
+*/
+void more_numbers(void);
+
+/**
+* print_line - draws a straight line in the terminal
+*/
+void print_line(int n);
+
+/**
+* print_diagonal - draws a diagonal line on the terminal
+*/
+void print_diagonal(int n);
+
+/**
+* print_square - prints a square followed by a new line
+*/
+void print_square(int size);
+
+/**
+* fizz_buzz - prints numbers from 1 to 100 followed by a new line
+* for multiples of three print Fizz
+* for multiples of five print Buzz
+* for numbers that are multiples of both three and five print FizzBuzz
+*/
+
+/**
+* print_triangle - prints a triangle followed by a new line
+*/
+void print_triangle(int size);
+
+#endif /* HOLBERTON_H */
