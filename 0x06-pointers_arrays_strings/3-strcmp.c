fd463029208334c44c19a41bbdd8a169be37a527
JudgeFudge19 <dremukare@gmail.com>
2021-02-18 17:27:32 +0000

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, output;
 
-	for (i = 0; *(s1 + i) != '\0' && *(s2 + i) != '\0'; i++)
-		if (*(s1 + i) != (*s2 + i))
-			break;
+	i = 0;
+	while (*(s1 + i) == *(s1 + i) && *(s1 + i) != '\0')
+		i++;
+
 	output = (*(s1 + i) - *(s2 + i));
 
 	return (output);
