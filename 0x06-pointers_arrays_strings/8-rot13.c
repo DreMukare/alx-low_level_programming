4d9014b9382169ee6c12ba7b951b3e100dff280f
JudgeFudge19 <dremukare@gmail.com>
2021-02-18 19:08:57 +0000

    encodes string into rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* rot13 - encodes a string into rot13
+* @s: string to be encoded
+*
+* Return: returns encoded string
+*/
+char *rot13(char *s)
+{
+	int i = 0, j;
+
+	char let[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char let13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (let[j] == s[i])
+			{
+				s[i] = let13[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
