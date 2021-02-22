54a0d05ad9e9a1d898e1694a5f4f7650efee444f
JudgeFudge19 <dremukare@gmail.com>
2021-02-22 20:52:10 +0000

    get sum of diagonal array elements

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* print_diagsums - prints sum of the diagonals of a square matrix
+* @a: matrix array
+* size: size of columns and rows
+*
+* Return: no return
+*/
+void print_diagsums(int *a, int size)
+{
+	int i, s = 0, sum = 0, sum1 = 0;
+
+	s = (size * size) - 1;
+	for (i = 0; i <= s; i = i + (size + 1))
+	{
+		sum = sum + a[i];
+	}
+	for (i = (size - 1); i < s; i = i + (size - 1))
+	{
+		sum1 = sum1 + a[i];
+	}
+	printf("%d, %d\n", sum, sum1);
+}
