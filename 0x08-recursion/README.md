6e12869517be5d8c823e479be0666d171cda352a
JudgeFudge19 <dremukare@gmail.com>
2021-02-24 13:39:54 +0000

    initial commit

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,45 @@
+# 0x08 - Recursion
+
+##0. _puts_recursion
+
+**Prints a string followed by a new line**
+
+##1. _print_rev_recursion
+
+**Prints string in reverse followed a new line**
+
+##2. _strlen_recursion
+
+**Returns the length of a string**
+
+##3. factorial
+
+**Returns the factorial of a given number**
+
+> - If `n` is lower than `0` the function should return `-1` to indicate an error
+> - Factorial of `0` is `1`
+
+##4. _pow_recursion
+
+**Returns the value of x raised to the power of y**
+
+> - If `y` is lower than `0` the function should return `-1`
+
+##5. _sqrt_recursion
+
+** Returns the natural square root of a number**
+
+> - If `n` does not have a natural square root the function should return `-1`
+
+##6. is_prime_number
+
+**Checks whether input integer is a prime number**
+
+> - Returns `1` if it is and `0` if it is not
+
+##7. _is_palindrome
+
+**CHecks whether a string is a palindrome**
+
+> - Returns `1` if it is and `0` if it is not
+
---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,49 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+* _putchar - writes the character c to stdout
+*/
+int _putchar(char c);
+
+/**
+* _puts_recursion - Prints a string followed by a new line
+*/
+void _puts_recursion(char *s);
+
+/**
+* _print_rev_recursion - Prints a string in reverse followed by a new line
+*/
+void _print_rev_recursion(char *s);
+
+/**
+* _strlen_recursion - returns the length of a string
+*/
+int _strlen_recursion(char *s);
+
+/**
+* factorial - returns the factorial of a given number
+*/
+int factorial(int n);
+
+/**
+* _pow_recursion - returns value of x raised to power of y
+*/
+int _pow_recursion(int x, int y);
+
+/**
+* _sqrt_recursion - returns the natural square root of a number
+*/
+int _sqrt_recursion(int n);
+
+/**
+* is_prime_number - returns 1 if input is prime number and 0 if not
+*/
+int is_prime_number(int n);
+
+/**
+* is_palindrome - returns 1 if input is palindrome and 0 if not
+*/
+int is_palindrome(char *s);
+
+#endif /* HOLBERTON_H */
