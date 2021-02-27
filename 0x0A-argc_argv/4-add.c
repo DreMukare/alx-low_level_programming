1d0bf43739149cc49a301b1dedff1e1b95c301d1
JudgeFudge19 <dremukare@gmail.com>
2021-02-27 10:53:10 +0000

    fixed error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,19 +12,24 @@
 */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
+	if (argc ==  1)
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (isdigit(*argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			sum += atoi(argv[i]);
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
