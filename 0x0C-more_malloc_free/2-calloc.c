54cb48ad7f8c897a96a7ca6cfc37c3a7ba2ab264
JudgeFudge19 <dremukare@gmail.com>
2021-03-04 20:21:08 +0000

    allocates memory for an array using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,9 +23,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	while (i < nmemb)
+	while (i < (nmemb * size))
 	{
-		ptr[i] = 0;
+		*((char *)(ptr) + i) = 0;
 		i++;
 	}
 
