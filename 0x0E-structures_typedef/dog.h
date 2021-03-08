a8bed0ebec0d3232a9397321f472a97c74a6eb55
JudgeFudge19 <dremukare@gmail.com>
2021-03-08 12:16:14 +0000

    structure definition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - such a good pupper
+* @name: name of doggo
+* @age: age of doggo
+* @owner: owner of doggo
+*
+* Description: This struct will be used to hold details for different dogs
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
