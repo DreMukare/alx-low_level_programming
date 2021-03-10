0b89f5b0acc19911aa362f55cfa9db9b13274a92
JudgeFudge19 <dremukare@gmail.com>
2021-03-10 20:59:18 +0000

    performs simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -14,15 +14,10 @@ typedef struct op
 } op_t;
 
 int op_add(int a, int b);
-
 int op_sub(int a, int b);
-
 int op_mul(int a, int b);
-
 int op_div(int a, int b);
-
 int op_mod(int a, int b);
-
 int (*get_op_func(char *s))(int, int);
 
 #endif /* CALC_H */
