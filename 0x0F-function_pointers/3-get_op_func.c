5cdf91e50b6944fac48e7dfa188417174beeddcd
JudgeFudge19 <dremukare@gmail.com>
2021-03-10 20:57:45 +0000

    performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,19 +8,19 @@
 */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t = ops[] = {
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-	}
+	};
 	int i = 0;
 
 	while (i < 5)
 	{
-		if (s[0] = ops[i].op[0])
+		if (s[0] == ops[i].op[0])
 			return (ops[i].f);
 		i++;
 	}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
 * main - calls other functions
