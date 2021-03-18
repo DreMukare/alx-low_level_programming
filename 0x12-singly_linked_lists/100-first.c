33f0a27d61cde2f53a9c4223db38c48f059e2490
JudgeFudge19 <dremukare@gmail.com>
2021-03-18 15:53:42 +0000

    first advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,23 +1,11 @@
 #include "lists.h"
 
-void print_before(void) __attribute__ ((constructor));
-
-int cleanup(void) __attribute__ ((desctructor));
-
 /**
 * print_before - prints string before main function
 * Return: no return
 */
 void print_before(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-
-/**
-* cleanup - desctructs
-* Return: 0
-*/
-int cleanup(void)
-{
-	return (1);
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
