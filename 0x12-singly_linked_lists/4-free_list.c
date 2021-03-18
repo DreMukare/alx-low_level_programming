ecda42c457718c5e917f030ae05b147574b7bece
JudgeFudge19 <dremukare@gmail.com>
2021-03-18 15:34:58 +0000

    created 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_list - frees a list_t list
+* @head: pointer to head node
+*
+* Return: no return
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
