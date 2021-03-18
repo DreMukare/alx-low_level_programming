9a6729be416541b7725437ab2e75998eb631b674
JudgeFudge19 <dremukare@gmail.com>
2021-03-18 14:41:30 +0000

    created 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* list_len - returns the number of elements in a linked list_t list
+* @h: pointer to the list
+*
+* Return: number of elements in a linked list_t list
+*/
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
