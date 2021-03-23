6fbaaf4ca1c9bafd4c495a9d7ba95f62a8c2fdca
JudgeFudge19 <dremukare@gmail.com>
2021-03-23 20:53:43 +0000

    deleted node at given index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		temp = temp->next;
 		count++;
-		if (!temp || !(temp->next))
+		if (!(temp) || !(temp->next))
 			return (-1);
 	}
 
