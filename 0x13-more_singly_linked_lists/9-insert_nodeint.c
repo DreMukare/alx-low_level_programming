2a0033b03770943d7a5eaa6ac250ce798beb48fd
JudgeFudge19 <dremukare@gmail.com>
2021-03-23 20:32:46 +0000

    added new node at given position in list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			*head = new_node;
 			return (new_node);
 		}
-		if (count + 1 = idx)
+		if (count + 1 == idx)
 		{
 			new_node->next = (*head)->next;
 			(*head)->next = new_node;
