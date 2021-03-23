9217e2aa10136086ac8113224c035b8f71eee1b9
JudgeFudge19 <dremukare@gmail.com>
2021-03-23 19:02:51 +0000

    inserted new node at beginning of linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,7 +9,7 @@
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t new_head = malloc(sizeof(listint_t));
+	listint_t *new_head = malloc(sizeof(listint_t));
 
 	if (!new_head)
 		return (NULL);
