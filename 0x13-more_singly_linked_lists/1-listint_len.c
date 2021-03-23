7793a54b676d7255e05e77258a8f4c4b615c47e1
JudgeFudge19 <dremukare@gmail.com>
2021-03-23 18:43:06 +0000

    returning number of elements in linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* listint_len - returns number of elements in linked list
+* @h: pointer to head node
+*
+* Return: number of elements in linked list
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while(h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
