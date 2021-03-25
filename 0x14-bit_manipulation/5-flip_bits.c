524a9e756836be62b8a64cd59d1dbe7a95a9a55f
JudgeFudge19 <dremukare@gmail.com>
2021-03-25 19:42:14 +0000

    counting number of bits that would need to be flipped to get from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* flip_bits - returns the number of bits needed to be flipped to get from one 
+* number to another
+* @n: the number to be converted to m
+* @m: number to be converted to
+*
+* Return: number of bits needed to be flipped to get from one number to another
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int x = n ^ m, count = 0;
+
+	while (x > 0)
+	{
+		count++;
+		x &= (x - 1);
+	}
+	return (count);
+}
