09aaedee02496ee888fdb8f960bb975edb5083df
Andrew Mukare <dremukare@gmail.com>
2021-03-29 21:12:58 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,8 +21,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]), exit(98);
 
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
 	while ((read_from = read(fd_from, buf, BUFSIZ)) > 0)
 	{
