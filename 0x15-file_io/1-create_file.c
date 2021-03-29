7a328f571cf1474df74007801e9b190746c85c3e
JudgeFudge19 <dremukare@gmail.com>
2021-03-29 14:49:19 +0000

    created file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+* create_file - creates a file
+* @filename: name of file
+* @text_content: text to go in file
+*
+* Return: 1 on success, -1 on failure
+* -1 if filename is NULL
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+	ssize_t written;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[len] != '\0')
+		len++;
+
+	if (text_content)
+		written = write(fd, (void *)text_content, len);
+	written = write(fd, NULL, 0);
+
+	if (written == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
