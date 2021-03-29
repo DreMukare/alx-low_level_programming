39ab6785d939767680b2e9e29d6f46746c8b62b1
JudgeFudge19 <dremukare@gmail.com>
2021-03-29 15:19:52 +0000

    created file using open sys call

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[len] != '\0')
+	while (text_content && text_content[len] != '\0')
 		len++;
 
 	if (!len)
