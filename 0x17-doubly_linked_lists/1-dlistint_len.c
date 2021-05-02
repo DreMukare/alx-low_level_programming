aca1e9c6cab3696f2071cbfb6e3bc6f29f2dde76
Andrew Mukare <dremukare@gmail.com>
2021-05-02 21:09:19 +0300

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,10 +8,9 @@
 */
 size_t dlistint_len(const dlistint_t *h)
 {
-	const dlistint_t temp;
-	size_t count;
+	const dlistint_t *temp;
+	size_t count = 0;
 
-	count = 0;
 	temp = h;
 	while (temp != NULL)
 	{
