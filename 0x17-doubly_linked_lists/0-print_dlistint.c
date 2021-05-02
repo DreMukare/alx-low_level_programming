9c29d1cb880836066f922fa9b137f96e7d30b0df
Andrew Mukare <dremukare@gmail.com>
2021-05-02 21:00:56 +0300

    fixed errors

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,17 +5,18 @@
 * @h: pointer to head of dlistint_t list
 * Return: number of nodes
 */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-		dlistint_t *temp;
-		size_t count = 0;
+	const dlistint_t *temp;
+	size_t count = 0;
 
-		temp = h;
-		while (temp != NULL)
-		{
-			printf("%d\n", temp->n);
-			count++;
-			temp = temp->next;
-		}
-		return (count);
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count++;
+	}
+	return (count);
 }
