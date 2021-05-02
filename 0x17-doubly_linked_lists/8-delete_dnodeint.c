43625cfe717d0ded0480c3dfedc9c82ea821c715
JudgeFudge19 <dremukare@gmail.com>
2021-05-02 19:39:54 +0000

    fixed betty errors

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,26 +9,26 @@
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-  dlistint_t *current = *head, *temp;
-  unsigned int count = 0;
-  
-  if (*head == NULL || len < index)
-    return (-1);
-  if (index == 0)
-    *head = (*head)->next;
-  else
-  {
-    while (count < (index - 1))
-    {
-      if (current == NULL)
-        return (-1);
-      current = current->next;
-      count++;
-    }
-    temp = current;
-    current = current->next;
-    temp->next = current == NULL ? NULL : current->next;
-  }
-  free(current);
-  return (1);
+	dlistint_t *current = *head, *temp;
+	unsigned int count = 0;
+
+	if (*head == NULL || len < index)
+		return (-1);
+	if (index == 0)
+		*head = (*head)->next;
+	else
+	{
+		while (count < (index - 1))
+		{
+			if (current == NULL)
+				return (-1);
+			current = current->next;
+			count++;
+		}
+		temp = current;
+		current = current->next;
+		temp->next = current == NULL ? NULL : current->next;
+	}
+	free(current);
+	return (1);
 }
