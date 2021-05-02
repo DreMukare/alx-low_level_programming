e522d8e9be041b2276f27c07d12019021bac7ca5
Andrew Mukare <dremukare@gmail.com>
2021-05-02 20:47:50 +0300

    created header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -18,7 +18,7 @@ typedef struct dlistint_s
 		int n;
 		struct dlistint_s *prev;
 		struct dlistint_s *next;
-}
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
