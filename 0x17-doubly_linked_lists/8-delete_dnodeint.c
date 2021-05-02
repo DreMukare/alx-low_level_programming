4c4a79760a69077bb606677f7f999f2349329607
Andrew Mukare <dremukare@gmail.com>
2021-05-02 22:28:41 +0300

    Create 8-delete_dnodeint.c

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* delete_dnodeint_at_index - deletes node at nth position
+* @head: pointer to pointer to head
+* @index: position
+*
+* Return: 1 if it succeeded, -1 if it failed
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+  
