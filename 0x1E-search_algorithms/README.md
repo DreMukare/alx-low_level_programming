a63e6ea6154fa9881523659e34092cb18a60a30f
Andrew Mukare <dremukare@gmail.com>
2021-09-01 17:21:08 +0000

    initial commit

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,3 @@
+# 0x1E. C - Search Algorithms
+
+This project goes over difference search algorithms and space complexity as a concept.
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGOS_H */
