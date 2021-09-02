eb2a5ebbffc20189d094f11e67a400bcbec456e6
Andrew Mukare <dremukare@gmail.com>
2021-09-02 13:08:34 +0000

    task 1: finding value in array using binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,33 +1,5 @@
 #include "search_algos.h"
 
-
-/**
-* print_array - prints an array
-*
-* @array: array to be printed
-* @start: start index to print from
-* @end: end index to print from
-*
-* Return: no return
-*/
-void print_array(int *array, int start, int end)
-{
-	int i = start;
-
-	while (i <= end)
-	{
-		if (i != end)
-		{
-			printf("%d, ", array[i]);
-		}
-		else
-			printf("%d", array[i]);
-
-		i++;
-	}
-	printf("\n");
-}
-
 /**
 * binary_search - searches for value in array of integers
 *
@@ -42,32 +14,32 @@ int binary_search(int *array, size_t size, int value)
 {
 	int right = array[size - 1];
 	int left = array[0];
-	int mid = 0;
+	int i, mid = 0;
 
 	if (!array)
 		return (-1);
 
 	while (left <= right)
 	{
+		printf("Searching in array: ");
+
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i != right)
+				printf(", ");
+			else
+				printf("\n");
+		}
+
 		mid = (left + right) / 2;
+
 		if (value == mid)
-		{
-			printf("Searching in array: ");
-			print_array(array, left, right);
 			return (mid);
-		}
 		else if (value > mid)
-		{
-			printf("Searching in array: ");
-			print_array(array, left, right);
 			left = mid + 1;
-		}
 		else
-		{
-			printf("Searching in array: ");
-			print_array(array, left, right);
 			right = mid - 1;
-		}
 	}
 	return (-1);
 }
